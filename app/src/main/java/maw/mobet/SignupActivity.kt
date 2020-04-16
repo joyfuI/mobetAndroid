a353b4edfdbae1bf9790526c315a0a471300c410
joyfuI <jong970105@gmail.com>
2020-04-16 22:15:37 +0900

    회원가입 전 애니메이션 추가

--- app/src/main/java/maw/mobet/SignupActivity.kt
+++ app/src/main/java/maw/mobet/SignupActivity.kt
@@ -2,7 +2,10 @@ package maw.mobet
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_signup.*
 import kotlinx.android.synthetic.main.custom_actionbar.*
 
 class SignupActivity : AppCompatActivity() {
@@ -10,12 +13,51 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
+        before.visibility = View.VISIBLE
+        after.visibility = View.GONE
+
         // 액션바
         changeTitle(app_title_txt, getString(R.string.signup_title))
         back_img.visibility = View.VISIBLE
         noti_img.visibility = View.GONE
         back_img.setOnClickListener {
             finish()
+        }
+    }
+
+    fun onclick(view: View) {
+        when (view) {
+            // 회원가입 전 다음 버튼
+            next_btn -> {
+                val outAnim = AnimationUtils.loadAnimation(this, R.anim.slide_out_top)
+                outAnim.setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationRepeat(p0: Animation?) {
+                    }
+
+                    override fun onAnimationEnd(p0: Animation?) {
+                        before.visibility = View.GONE
+                    }
+
+                    override fun onAnimationStart(p0: Animation?) {
+                        next_btn.isClickable = false
+                    }
+                })
+                before.startAnimation(outAnim)
+
+                val inAnim = AnimationUtils.loadAnimation(this, R.anim.slide_in_bottom)
+                inAnim.setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationRepeat(p0: Animation?) {
+                    }
+
+                    override fun onAnimationEnd(p0: Animation?) {
+                    }
+
+                    override fun onAnimationStart(p0: Animation?) {
+                        after.visibility = View.VISIBLE
+                    }
+                })
+                after.startAnimation(inAnim)
+            }
         }
     }
 }
