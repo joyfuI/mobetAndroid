ef767bb023e79fb7249ea5a30859304cbf3d72ff
limdoyeon <dodocat52@naver.com>
2020-05-14 01:47:40 +0900

    commit to mypage fragment

--- app/src/main/java/maw/mobet/ui/my/MyFragment.kt
+++ app/src/main/java/maw/mobet/ui/my/MyFragment.kt
@@ -6,15 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.fragment_my.*
 import maw.mobet.R
+import maw.mobet.ui.my.achievement.Fragment_finish
+import maw.mobet.ui.my.statistics.Fragment_playing
+import androidx.fragment.app.FragmentManager as FragmentManager
 
 class MyFragment : Fragment() {
     private lateinit var viewModel: MyViewModel
+    private val fragmentPlaying = Fragment_playing.newInstance()
+    private val fragmentFinish = Fragment_finish.newInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         viewModel = ViewModelProvider(this)[MyViewModel::class.java]
+        view_pager.adapter = PagerAdapter(this, listOf(
+            fragmentPlaying,
+            fragmentFinish
+        ))
+
         return inflater.inflate(R.layout.fragment_my, container, false)
     }
 
