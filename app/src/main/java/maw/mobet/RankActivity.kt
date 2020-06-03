3f8b721add6f8c6064283967fbf759cf5146ba83
joyfuI <jong970105@gmail.com>
2020-06-03 23:35:18 +0900

    개인 랭크 뷰에 액션바 추가

--- app/src/main/java/maw/mobet/RankActivity.kt
+++ app/src/main/java/maw/mobet/RankActivity.kt
@@ -1,12 +1,16 @@
 package maw.mobet
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_rank_personal.*
+import kotlinx.android.synthetic.main.custom_actionbar.*
 import maw.mobet.api.RankItem
 import maw.mobet.ui.my.finish.PersonalViewModel
+import splitties.activities.start
+import splitties.resources.appStr
 
 class RankActivity : AppCompatActivity() {
     private lateinit var viewModel: PersonalViewModel
@@ -17,11 +21,24 @@ class RankActivity : AppCompatActivity() {
         setContentView(R.layout.activity_rank_personal)
         viewModel = ViewModelProvider(this)[PersonalViewModel::class.java]
 
+        // 액션바
+        back_img.visibility = View.VISIBLE
+        back_img.setOnClickListener {
+            finish()
+        }
+        noti_img.setOnClickListener {
+            start<NotifyActivity>()
+        }
+
         viewModel.loadData()
         viewModel.rank.observe(this, Observer {
             rank = it
-            progress_grade.progress = rank.nextRank.toInt()
 
+            var title = appStr(R.string.personal_title)
+            title = title.replace("{0}", rank.my.nick)
+            changeTitle(app_title_txt, title)
+
+            progress_grade.progress = rank.nextRank.toInt()
             progress_rank.progress = rank.toppercent.toInt()
             grade_txt.text = rank.my.grade
             val text = "다음 등급까지 " + "${rank.nextRank} %"
